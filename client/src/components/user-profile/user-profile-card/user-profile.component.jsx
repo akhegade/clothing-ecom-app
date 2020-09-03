187f078051022d1403711ddc768bf4250c029edd
akheggde <anilhegde006@gmail.com>
2020-09-03 16:49:55 +0530

    added UserIcon in profile and modified some code

--- client/src/components/user-profile/user-profile-card/user-profile.component.jsx
+++ client/src/components/user-profile/user-profile-card/user-profile.component.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 
+import UserIcon from "../user-icon/user-icon.component";
 import "./user-propfile.style.scss";
 
-const UserProfile = ({user: {displayName, email, emailVerified}, children}) => (
-  <div className="user-profile">
-    <span>{displayName.toUpperCase()}</span>
-    <span className="">
-      {email}
+const UserProfile = ({user, children}) => {
+  const {displayName, email, emailVerified} = user;
+  return (
+    <div className="user-profile">
+      <div className="user-details">
+        <UserIcon user={user} />
+        <div className="user-info">
+          <span>{displayName.toUpperCase()}</span>
+          <span className="email">
+            {email}
 
-      {emailVerified ? (
-        <span className="verfied">Verified</span>
-      ) : (
-        <span className="not-verified">
-          <br />
-          Please verify your email
-        </span>
-      )}
-    </span>
-    {children}
-  </div>
-);
+            {emailVerified ? (
+              <span className="verfied">Verified</span>
+            ) : (
+              <span className="not-verified">Please verify your email</span>
+            )}
+          </span>
+        </div>
+      </div>
+      <hr />
+      {children}
+    </div>
+  );
+};
 
 export default UserProfile;
